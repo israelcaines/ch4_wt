a272b6c05902302062700251c1b5751fe12d0dec
Israel <icaines@uncc.edu>
2016-10-11 01:24:23 +0000

    Done with section 4.7.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,6 +13,7 @@ end
 # in movies_controller.rb
 def create
   @movie = Movie.create!(params[:movie])
+  flash[:notice] = "#{@movie.title} was successfully created."
   redirect_to movies_path
 end
 
